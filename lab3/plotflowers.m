ae14979c61c0dbf9c260cf931ad5f4359c1d398e
Ankur Shukla <ankur.shukla.7818@student.uu.se>
2018-03-20 15:20:53 +0100

    figure code added

--- lab3/plotflowers.m
+++ lab3/plotflowers.m
@@ -3,3 +3,13 @@ load iris_dataset;
 somiris = newsom(irisInputs, [10 10], 'hextop', 'linkdist', 200, 3);
 somiris.trainParam.epochs = 100; % 100 seems good
 [trained_som, stats] = train(somiris, irisInputs);
+
+figure(10)
+plotsomhits(somiris, irisInputs(:,1:50)); %winning node for class I
+title('Class 1');
+figure(11)
+plotsomhits(somiris, irisInputs(:,51:100)); %winning node for class II
+title('Class 2');
+figure(12)
+plotsomhits(somiris, irisInputs(:,101:150)); %winning node for class III
+title('Class 3');
