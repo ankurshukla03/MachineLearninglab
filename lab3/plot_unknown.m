249ebb05d97ac027f28416ed646079c5e50a823c
Ankur Shukla <ankur.shukla.7818@student.uu.se>
2018-03-20 16:56:47 +0100

    changes

--- lab3/plot_unknown.m
+++ lab3/plot_unknown.m
@@ -6,4 +6,19 @@ somunknown = newsom(unknown_data,[10 10],'hextop','linkdist',300,10);
 somunknown.trainParam.epochs = 300;
 [trained_som, stats] = train(somunknown, unknown_data);
 
-plotsomhits(trained_som,unknown_data(:,:));
+%plotsomhits(trained_som,unknown_data(:,:));
+figure(10)
+plotsomhits(trained_som, point1); %winning node for class I
+title('Class 1');
+figure(11)
+plotsomhits(trained_som, point2); %winning node for class II
+title('Class 2');
+figure(12)
+plotsomhits(trained_som, point3); %winning node for class III
+title('Class 3');
+figure(13)
+plotsomhits(trained_som, point4); %winning node for class III
+title('Class 4');
+figure(14)
+plotsomhits(trained_som, point5); %winning node for class III
+title('Class 5');
