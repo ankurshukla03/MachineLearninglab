c281c89c83480eb8470859aaa929fc61c806585c
Ankur Shukla <ankur.shukla.7818@student.uu.se>
2018-03-20 16:30:30 +0100

    minor change

--- lab3/plot_unknown.m
+++ lab3/plot_unknown.m
@@ -6,4 +6,4 @@ somunknown = newsom(unknown_data,[10 10],'hextop','linkdist',300,10);
 somunknown.trainParam.epochs = 300;
 [trained_som, stats] = train(somunknown, unknown_data);
 
-plotsomhits(trained_som,somunknown(:,:));
+plotsomhits(trained_som,unknown_data(:,:));
