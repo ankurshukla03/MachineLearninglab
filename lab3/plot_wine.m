6951e85d5331dee542d1f905da19e7952a7d08b5
Tristan <thrawst88@gmail.com>
2018-03-20 15:05:20 +0100

    figure the plotsomhits

--- lab3/plot_wine.m
+++ lab3/plot_wine.m
@@ -1,12 +1,15 @@
 clear all;
 load wine_dataset;
 
-somwine = newsom(wineInputs, [5 5], 'hextop', 'linkdist', 100, 5);
+somwine = newsom(wineInputs, [10 10], 'hextop', 'linkdist', 100, 3);
 
 somwine.trainParam.epochs = 100;
 [som_wine, stats] = train(somwine, wineInputs);
 
-%plotsomhits(som_wine, wineInputs(:,1:59))%winning node for class I
-%plotsomhits(som_wine, wineInputs(:,60:130))%winning node for class II
-%plotsomhits(som_wine, wineInputs(:,131:178))%winning node for class III
-plotsomhits(som_wine, wineInputs(:,:));
+figure(10)
+plotsomhits(som_wine, wineInputs(:,1:59)); %winning node for class I
+figure(11)
+plotsomhits(som_wine, wineInputs(:,60:130)); %winning node for class II
+figure(12)
+plotsomhits(som_wine, wineInputs(:,131:178)); %winning node for class III
+% plotsomhits(som_wine, wineInputs(:,:));
