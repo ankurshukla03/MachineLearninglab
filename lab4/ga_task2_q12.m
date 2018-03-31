848b166e88c4d1437c055e6561d4b5fd465bf084
Tristan <thrawst88@gmail.com>
2018-03-31 15:08:25 +0200

    mutation experiments

--- lab4/ga_task2_q12.m
+++ lab4/ga_task2_q12.m
@@ -11,20 +11,28 @@ ack.visual.interval = 0.05;
 
 ack.stop.direction = 'min';
 ack.visual.func = 'ackley';
-%ack.mutate.proportional = true;%second strategy for mutation operator
-ack.mutate.prob = 0.5;
 
-[best, fit, stat] = GAsolver(20, [-32, 32], ...
+bests = zeros(numel(funcs), 1);
+diversities = zeros(numel(funcs), 251);
+funcs = {'none', 'linear', 'exponential'};
+ack.mutate.prob = 0.25;
+ack.mutate.proportional = true; % default false
+% probs = [0.001 0.005 0.01 0.025 0.05 0.075 0.1 0.25 0.5 0.75 0.9];
+for i=1:numel(funcs)
+    ack.mutate.decay = funcs{i};
+    [best, fit, stat] = GAsolver(20, [-32, 32], ...
                              'ackley', 200, 250, ack);
- 
+    bests(i) = stat.eliteFit(1);
+    diversities(i,:) = stat.diversity;
+end
 
 %% print 
-% close all;
-% figure(10)
-% hold on
-% for j=1:numel(funcs)
-%    fprintf("%s best fit %0.2f \n", funcs{j}, bests(j)); 
-%    plot(diversities(j,:), 'DisplayName', funcs{j});
-% end
-% legend()
-% hold off
+close all;
+figure(10)
+hold on
+for j=1:numel(funcs)
+   fprintf("%s best fit: %0.3f \n", funcs{j}, bests(j)); 
+   plot(diversities(j,:), 'DisplayName', funcs{j});
+end
+legend()
+hold off
