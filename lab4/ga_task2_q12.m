67f3db2dc0b8f0762a2084d20ad9b767332a304e
Ankur Shukla <ankur.shukla.7818@student.uu.se>
2018-03-31 14:56:42 +0200

    mutation operators

--- lab4/ga_task2_q12.m
+++ lab4/ga_task2_q12.m
@@ -11,6 +11,8 @@ ack.visual.interval = 0.05;
 
 ack.stop.direction = 'min';
 ack.visual.func = 'ackley';
+%ack.mutate.proportional = true;%second strategy for mutation operator
+ack.mutate.prob = 0.5;
 
 [best, fit, stat] = GAsolver(20, [-32, 32], ...
                              'ackley', 200, 250, ack);
