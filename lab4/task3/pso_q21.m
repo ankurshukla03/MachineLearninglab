9b8dd49c842f5b86797dd26f65dc00c959301207
Ankur Shukla <ankur.shukla.7818@student.uu.se>
2018-04-01 14:37:04 +0200

    q21 task3 pso

---
+++ lab4/task3/pso_q21.m
@@ -0,0 +1,15 @@
+close all;
+load lab4.mat;
+%question 21 constriction coefficients
+
+options = psooptimset('DemoMode','fast',...
+'InertiaWeight',1,'PlotFcns',{@psoplotswarmsurf,@psoplotbestf,@psoplotvelocity},...
+'PopInitRange',[-32;32] ,...
+'InertiaWeight'      , [0.8,0.5], ... % weight
+'SocialAttraction'   , 1.8, ... % c1
+'CognitiveAttraction', 1.8);    % c2
+
+options = psooptimset(options, 'Generations', 500);
+options = psooptimset(options,'VelocityLimit',1);
+
+[x, fval] = pso(@ackleysfcn,20,[],[],[],[],[],[],[],options);
