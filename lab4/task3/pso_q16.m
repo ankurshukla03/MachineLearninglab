3d9a9596393024a155efab9d01b735a02414f8e3
Ankur Shukla <ankur.shukla.7818@student.uu.se>
2018-04-01 13:14:59 +0200

    PSO q16 Task3

---
+++ lab4/task3/pso_q16.m
@@ -0,0 +1,9 @@
+close all;
+load lab4.mat;
+
+options = psooptimset('DemoMode','fast',...
+'PlotFcns',{@psoplotswarmsurf,@psoplotbestf},...
+'PopInitRange',[-32;32],'InertiaWeight',1,...
+'SocialAttraction',2,'CognitiveAttraction',2);
+
+[x, fval] = pso(@ackleysfcn,20,[],[],[],[],[],[],[],options);
