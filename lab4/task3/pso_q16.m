00df0415c6876bc86fe36913cf3413f2dec52931
Ankur Shukla <ankur.shukla.7818@student.uu.se>
2018-04-01 13:30:07 +0200

    plot for velocit added

--- lab4/task3/pso_q16.m
+++ lab4/task3/pso_q16.m
@@ -2,12 +2,11 @@ close all;
 load lab4.mat;
 
 options = psooptimset('DemoMode','fast',...
-'PlotFcns',{@psoplotswarmsurf,@psoplotbestf},...
+'PlotFcns',{@psoplotswarmsurf,@psoplotbestf,@psoplotvelocity},...
 'PopInitRange',[-32;32],'InertiaWeight',1,...
 'SocialAttraction',2,'CognitiveAttraction',2);
 
 options = psooptimset(options, 'Generations', 500);
 options = psooptimset(options,'VelocityLimit',30);
-
 
 [x, fval] = pso(@ackleysfcn,20,[],[],[],[],[],[],[],options);
