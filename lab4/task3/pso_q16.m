6747b9cb5e45d90707befeafa8143d479dc6fdef
Ankur Shukla <ankur.shukla.7818@student.uu.se>
2018-04-01 13:27:23 +0200

    veloctiy limit added

--- lab4/task3/pso_q16.m
+++ lab4/task3/pso_q16.m
@@ -4,9 +4,10 @@ load lab4.mat;
 options = psooptimset('DemoMode','fast',...
 'PlotFcns',{@psoplotswarmsurf,@psoplotbestf},...
 'PopInitRange',[-32;32],'InertiaWeight',1,...
-'SocialAttraction',2,'CognitiveAttraction',2,'VeloctiyLimit',30);
+'SocialAttraction',2,'CognitiveAttraction',2);
 
 options = psooptimset(options, 'Generations', 500);
+options = psooptimset(options,'VelocityLimit',30);
 
 
-[x, fval] = pso(@ackleysfcn,@psoplotvelocity,20,[],[],[],[],[],[],[],options);
+[x, fval] = pso(@ackleysfcn,20,[],[],[],[],[],[],[],options);
