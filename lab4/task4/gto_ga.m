9ddb90b68eab962ce16f84630d67f4d574955b98
Tristan <thrawst88@gmail.com>
2018-04-03 14:25:16 +0200

    final set for gto_ga

--- lab4/task4/gto_ga.m
+++ lab4/task4/gto_ga.m
@@ -5,7 +5,6 @@ mgapar.objParams.problem = MGADSMproblem;
 
 % parameters
 mgapar.select.func = 'proportional';
-mgapar.select.pressure = 1;
 
 mgapar.mutate.prob = 0.2;
 mgapar.mutate.decay = 'exponential';
@@ -13,10 +12,15 @@ mgapar.crossover.func = 'blend';
 % bests = zeros(100, 1);
 
 % for i=1:100 % mean = 28.141, best ind = 79 -> 15.0349
-rng(79);
+rng(79)
+% rng(i);
 [best, fit, stat] = GAsolver(22, PopInitRange', 'mgadsm', ...
                              50, 100, mgapar);
 [verification, lower, upper] = verify(best, MGADSMproblem);
-% bests(i) = fit;
+% if verification == 1
+%     bests(i) = fit;
+% else 
+%     bests(i) = inf;
+% end
 % end                         
                                     
