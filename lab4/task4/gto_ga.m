16a2204bb00bbd1143592ad4a7bf669811074149
Tristan <thrawst88@gmail.com>
2018-04-03 13:35:50 +0200

    improvements to gto

--- lab4/task4/gto_ga.m
+++ lab4/task4/gto_ga.m
@@ -8,15 +8,11 @@ mgapar.select.func = 'proportional';
 mgapar.select.pressure = 1;
 
 mgapar.mutate.prob = 0.2;
-mgapar.mutate.step = 0.25;
 mgapar.mutate.decay = 'exponential';
 mgapar.crossover.func = 'blend';
-mgapar.crossover.prob = 0.75;
-% mgapar.replace.comparative = true;
-% mgapar.replace.elitist = false; % default is true
 % bests = zeros(100, 1);
 
-% for i=1:100 % mean = 28.141, best ind = 79.
+% for i=1:100 % mean = 28.141, best ind = 79 -> 15.0349
 rng(79);
 [best, fit, stat] = GAsolver(22, PopInitRange', 'mgadsm', ...
                              50, 100, mgapar);
