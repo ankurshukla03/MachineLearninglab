3566486e4a70e2cc725371d3bb8d08ad3a69b165
Ankur Shukla <ankur.shukla.7818@student.uu.se>
2018-04-03 14:53:59 +0200

    rng generation

--- lab4/task4/gto_pso.m
+++ lab4/task4/gto_pso.m
@@ -9,10 +9,13 @@ options = psooptimset(options,'VeloctiyLimit',1.3);
 options = psooptimset(options,'InertiaWeight',[0.8,0.5]);
 options = psooptimset(options,'SocialAttraction',1);
 options = psooptimset(options,'CognitiveAttraction',1);
-options = psooptimset(options,'PlotFcns',{@psoplotswarmsurf,@psoplotbestf,@psoplotvelocity});
-bests = zeros(100, 1);
+%options = psooptimset(options,'PlotFcns',{@psoplotswarmsurf,@psoplotbestf,@psoplotvelocity});
+n = 50;
+bests = zeros(n, 1);
 
-for i=1:50
+for i=1:n
+    rng(i)
     [x,fval] = pso(@pso_gtop_lab4,22,[],[],[],[],LB,UB,[],options);
+    [verification, lower, upper] = verify(x, MGADSMproblem);
     bests(i) = fval;
 end
