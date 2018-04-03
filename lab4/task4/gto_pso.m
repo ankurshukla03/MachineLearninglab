7902cd4390edfeb958ef6265e609b5cd19241872
Ankur Shukla <ankur.shukla.7818@student.uu.se>
2018-04-03 14:25:42 +0200

    pso loop

--- lab4/task4/gto_pso.m
+++ lab4/task4/gto_pso.m
@@ -4,11 +4,15 @@ options = pso_gtop_lab4('init');
 LB = options.PopInitRange(1,:);
 UB = options.PopInitRange(2,:);
 
-options = psooptimset(options,'Generations',1000);
-options = psooptimset(options,'VeloctiyLimit',1);
+options = psooptimset(options,'Generations',500);
+options = psooptimset(options,'VeloctiyLimit',1.3);
 options = psooptimset(options,'InertiaWeight',[0.8,0.5]);
 options = psooptimset(options,'SocialAttraction',1);
 options = psooptimset(options,'CognitiveAttraction',1);
 options = psooptimset(options,'PlotFcns',{@psoplotswarmsurf,@psoplotbestf,@psoplotvelocity});
+bests = zeros(100, 1);
 
-[x,fval] = pso(@pso_gtop_lab4,22,[],[],[],[],LB,UB,[],options);
+for i=1:50
+    [x,fval] = pso(@pso_gtop_lab4,22,[],[],[],[],LB,UB,[],options);
+    bests(i) = fval;
+end
